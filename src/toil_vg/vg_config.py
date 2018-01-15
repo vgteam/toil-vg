c7a60e22bb5c409c4898eefcb7f5216503655637
Glenn Hickey <glenn.hickey@gmail.com>
2018-01-15 15:16:35 -0500

    default to more realistic simulation options

--- src/toil_vg/vg_config.py
+++ src/toil_vg/vg_config.py
@@ -230,7 +230,7 @@ vcfeval-opts: ['--ref-overlap', '--vcf-score-field', 'QUAL']
 #########################
 ### sim and mapeval Arguments ###
 # Options to pass to vg sim (should not include -x, -n, -s or -a)
-sim-opts: ['-l', '150', '-p', '500', '-v', '50', '-e', '0.01', '-i', '0.002']
+sim-opts: ['-l', '150', '-p', '570', '-v', '165', '-e', '0.01', '-i', '0.002']
 
 # Options to pass to bwa
 bwa-opts: []
@@ -453,7 +453,7 @@ vcfeval-opts: ['--ref-overlap', '--vcf-score-field', 'QUAL']
 #########################
 ### sim and mapeval Arguments ###
 # Options to pass to vg sim (should not include -x, -n, -s or -a)
-sim-opts: ['-l', '150', '-p', '500', '-v', '50', '-e', '0.01', '-i', '0.002']
+sim-opts: ['-l', '150', '-p', '570', '-v', '165', '-e', '0.01', '-i', '0.002']
 
 # Options to pass to bwa
 bwa-opts: []
