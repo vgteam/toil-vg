cb1d0b15b8b46efc354439a50ffbb32ad4ad2fce
Glenn Hickey <glenn.hickey@gmail.com>
2018-03-05 22:11:58 -0500

    update default vg docker image

--- src/toil_vg/vg_config.py
+++ src/toil_vg/vg_config.py
@@ -128,7 +128,7 @@ container: """ + ("Docker" if test_docker() else "None") + """
 ##   of through docker. 
 
 # Docker image to use for vg
-vg-docker: 'quay.io/vgteam/vg:v1.6.0-621-g766ecbcb-t141-run'
+vg-docker: 'quay.io/vgteam/vg:v1.6.0-654-g1c819fff-t142-run'
 
 # Docker image to use for bcftools
 bcftools-docker: 'vandhanak/bcftools:1.3.1'
@@ -349,7 +349,7 @@ container: """ + ("Docker" if test_docker() else "None") + """
 ##   of through docker. 
 
 # Docker image to use for vg
-vg-docker: 'quay.io/vgteam/vg:v1.6.0-621-g766ecbcb-t141-run'
+vg-docker: 'quay.io/vgteam/vg:v1.6.0-654-g1c819fff-t142-run'
 
 # Docker image to use for bcftools
 bcftools-docker: 'vandhanak/bcftools:1.3.1'
