df6f1f675aefaabeb84a8a4aaa6f6bf6fc2eb34f
Adam Novak <anovak@soe.ucsc.edu>
2018-05-25 14:12:54 -0700

    Make the plotting tool able to load calleval ROCs

--- src/toil_vg/vg_plot.py
+++ src/toil_vg/vg_plot.py
@@ -22,6 +22,7 @@ from toil.realtimeLogger import RealtimeLogger
 from toil_vg.vg_common import require, make_url, remove_ext,\
     add_common_vg_parse_args, add_container_tool_parse_args
 from toil_vg.vg_mapeval import run_map_eval_summarize, run_map_eval_table, run_map_eval_plot
+from toil_vg.vg_calleval import run_calleval_plots
 from toil_vg.context import Context, run_write_info_to_outstore
 
 logger = logging.getLogger(__name__)
@@ -54,8 +55,16 @@ def add_plot_options(parser):
     # General options
     parser.add_argument('--position-stats', type=make_url, default=None,
                         help='position.results.tsv file from a mapeval run')
+                        
+    parser.add_argument('--roc-base',
+                        help='ROC file base URL, under which <condition>_vcfeval_output/<type>_roc.tsv.gz files exist')
+    parser.add_argument('--names', nargs='+',
+                        help='condition names for ROC files to load')
+    parser.add_argument('--clipping', nargs='+', default=['clipped'],
+                        help='clipping modes to load')
+                        
     parser.add_argument('--plot-sets', nargs='+', default=[],
-                        help='comma-separated lists of condition-tagged GAM names (primary-mp-pe, etc.) to plot together')
+                        help='comma-separated lists of condition names (primary-mp-pe, etc.) to plot together')
     parser.add_argument('--tables-only', action='store_true',
                         help='make only summary tables and not plots')
     parser.add_argument('--plots-only', action='store_true',
@@ -72,27 +81,51 @@ def validate_options(options):
     Throw an error if an invalid combination of options has been selected.
     """
 
-    require(options.position_stats, 'a --position-stats file is required')
     
-def run_plot(job, context, options, position_stats_file_id, plot_sets):
+    require(options.position_stats or options.roc_base, 'either --position-stats or --roc-base is required')
+    require(not (options.position_stats and options.roc_base), 'cannot operate on --position-stats and --roc-base in the same run')
+    require(options.roc_base is None or options.names is not None, '--roc-base requires --names')
+    require(options.names is None or options.roc_base is not None, '--names requires --roc-base')
+    
+    
+def run_plot(job, context, options, position_stats_file_id=None, eval_results_dict=None, plot_sets=[None]):
     """
-    Main Toil job, and main entrypoint for use as a library.
+    Main Toil job.
     
-    Plot plots based on the given position_stats_file_id stats file,
-    restricting to the given set of sets of conditions to plot.
+    If position_stats_file_id is given, make mapping plots and/or tables based
+    on the given position_stats_file_id stats file, restricting to the given
+    set of sets of conditions to plot.
+    
+    If instead eval_results_dict is given, as a dict from condition name, then
+    'clipped'/'unclipped', then 'snp'/'non_snp'/'weighted' to ROC data file ID,
+    then plot the variant calling ROC curves.
     
     """
     
-    # Do plots and tables, unless we want to do just tables
-    job_fn = run_map_eval_summarize
-    if options.tables_only:
-        job_fn = run_map_eval_table
-    if options.plots_only:
-        job_fn = run_map_eval_plot
+    if position_stats_file_id is not None:
+        # Do position stats
     
-    plot_job = job.addChildJobFn(job_fn, context, position_stats_file_id, plot_sets,
-                                 cores=context.config.misc_cores, memory=context.config.misc_mem,
-                                 disk=context.config.misc_disk)
+        # Do plots and tables, unless we want to do just tables
+        job_fn = run_map_eval_summarize
+        if options.tables_only:
+            job_fn = run_map_eval_table
+        if options.plots_only:
+            job_fn = run_map_eval_plot
+        
+        plot_job = job.addChildJobFn(job_fn, context, position_stats_file_id, plot_sets,
+                                     cores=context.config.misc_cores, memory=context.config.misc_mem,
+                                     disk=context.config.misc_disk)
+                                     
+    elif eval_results_dict is not None:
+        # Do variant calling plotting
+        
+        # Get the names back
+        names = eval_results_dict.keys()
+        
+        plot_job = job.addChildJobFn(run_calleval_plots, context, names, eval_results_dict, plot_sets=plot_sets)
+    
+    else:
+        raise RuntimeError('No position stats or vcfeval results available!')
 
 def make_plot_plan(toil, options):
     """
@@ -114,8 +147,33 @@ def make_plot_plan(toil, options):
         # We will use a position stats file with all the reads to re-plot and
         # their correctness statuses and MAPQs
         plan.position_stats_file_id = toil.importFile(options.position_stats)
-    else:
-        plan.position_stats_file_id = None
+    
+    
+    if options.roc_base:
+        # We will load up a bunch of ROCs
+        
+        plan.eval_results_dict = collections.defaultdict(lambda: collections.defaultdict(dict))
+        
+        for condition in options.names:
+            # For each condition that should have a ROC
+            
+            for clipping in options.clipping:
+                # For each clipping mode
+                
+                # What should the condition be with the clip tag?
+                # If we are running just one mode it is just the condition.
+                # But if we are running both it is the condition and the condition-unclipped
+                clip_tag_condition = condition
+                if clipping == 'unclipped' and 'clipped' in options.clipping:
+                    clip_tag_condition += '-unclipped'
+                    
+                for roc_name in ['snp', 'non_snp', 'weighted']:
+                
+                    # What URL do we want
+                    url = make_url(options.roc_base + '/' + clip_tag_condition + '_vcfeval_output/' + roc_name + '_roc.tsv.gz')
+                    
+                    # Load it up in the appropriate spot
+                    plan.eval_results_dict[condition][clipping][roc_name] = toil.importFile(url)
    
     # Also process options that need parsing
     # TODO: Make mapeval include the plot sets in its plan as well?
