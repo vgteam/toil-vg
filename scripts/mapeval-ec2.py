1f8f4f57e28261a273824c81bff4f81bb44610e5
Glenn Hickey <glenn.hickey@gmail.com>
2018-06-12 11:35:37 -0400

    make ec2 mapeval script more flexible

--- scripts/mapeval-ec2.py
+++ scripts/mapeval-ec2.py
@@ -10,16 +10,14 @@ def parse_args(args):
         formatter_class=argparse.RawDescriptionHelpFormatter)    
     parser.add_argument("job_store")
     parser.add_argument("out_store")
-    parser.add_argument("basename", help="input file prefix.  will look for [outstore]/[basename].xg/gcsa,"
-                        " [basename]_primary.xg/gcsa etc. as made by construct-hs37d5-ec2.py")
     parser.add_argument("truth_reads", help="annotated gam or bam or fastq to use as truth. if not bam, .pos file needed too")
     parser.add_argument("--leader", help="name of leader created with create-ec2-leader.sh")    
     parser.add_argument("--config", help="path of config on leader")
     parser.add_argument("--fasta", help="fasta. ideally already indexed with bwa index", required=True)
-    parser.add_argument("--names", nargs="*", default=["primary", "minaf_0.034"],
-                        help="related graphs to try in addtion to basename"),
     parser.add_argument("--restart", action="store_true", help="resume toil workflow")
     parser.add_argument("--outname", required=True, help="subdirectory in outstore for all output")
+    parser.add_argument("--index_bases", required=True, nargs='+', help="locations of input indexes (no extension)")
+    parser.add_argument("--names", required=True, nargs='+', help="names of indexes")
     args = args[1:]
     return parser.parse_args(args)
 options = parse_args(sys.argv)
@@ -29,10 +27,11 @@ if not options.job_store.startswith('aws:'):
 if not options.out_store.startswith('aws:'):
     options.out_store = 'aws:us-west-2:{}'.format(options.out_store)
 
+basename = os.path.basename(options.index_bases[0])
 s3_outstore = options.out_store.replace('aws:us-west-2:', 's3://')
 me_outstore = os.path.join(options.out_store, options.outname)
     
-log_name = 'mapeval_{}.log'.format(os.path.basename(options.basename))
+log_name = 'mapeval_{}.log'.format(os.path.basename(basename))
 os_log_name = os.path.join(s3_outstore, options.outname, os.path.basename(log_name))
 
 cmd = ['mapeval', options.job_store, me_outstore,
@@ -42,26 +41,22 @@ cmd = ['mapeval', options.job_store, me_outstore,
        '--multipath']
 
 # make basename absolute
-absolute_basename = os.path.join(s3_outstore, options.basename)
-absolute_truth = os.path.join(s3_outstore, options.truth_reads)
+absolute_basename = os.path.join(s3_outstore, basename)
 
 if options.truth_reads.endswith('.bam'):
-    cmd += ['--bam_input_reads', absolute_truth]
+    cmd += ['--bam_input_reads', options.truth_reads]
 else:
     if options.truth_reads.endswith('.gam'):
-        cmd += ['--gam_input_reads', absolute_truth]
+        cmd += ['--gam_input_reads', options.truth_reads]
     else:
-        cmd += ['--fastq', absolute_truth]
-    cmd += ['--truth', os.path.splitext(absolute_truth)[0] + '.pos']
+        cmd += ['--fastq', options.truth_reads]
+    true_pos = options.truth_reads
+    if true_pos.endswith('.fq.gz'):
+        true_pos = true_pos[:-3]
+    cmd += ['--truth', os.path.splitext(true_pos)[0] + '.pos']
 
-cmd += ['--index-bases', absolute_basename]
-for name in options.names:
-    if name == 'primary':
-        cmd += [os.path.join(os.path.dirname(absolute_basename), name)]
-    else:
-        cmd += [absolute_basename + '_' + name]
-
-cmd += ['--gam-names', os.path.basename(options.basename)] + options.names
+cmd += ['--index-bases'] + options.index_bases
+cmd += ['--gam-names'] + options.names
 
 if 'ekg' in options.truth_reads:
     cmd += ['--ignore-quals']
