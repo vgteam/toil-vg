c14a83faf988bb9d19853e1e1319a1a1fb56dd20
Glenn Hickey <glenn.hickey@gmail.com>
2019-09-04 11:00:04 -0400

    missing comma

--- src/toil_vg/test/test_vg.py
+++ src/toil_vg/test/test_vg.py
@@ -469,7 +469,7 @@ class VGCGLTest(TestCase):
         '''
         self._run(['toil-vg', 'call', self.jobStoreLocal,
                    '--container', self.containerType,
-                   '--clean', 'never', '--old_call'
+                   '--clean', 'never', '--old_call',
                    self.xg_index, 'NA12877', outstore, '--gams', self.sample_gam,
                    '--chroms', '17', '13', '--vcf_offsets', '43044293', '32314860',
                    '--call_chunk_size', '23000', '--calling_cores', '4',
