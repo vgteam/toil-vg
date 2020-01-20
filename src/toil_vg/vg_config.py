6a1eb0842c051df5848c527755328f77976677b9
Glenn Hickey <glenn.hickey@gmail.com>
2020-01-20 15:45:16 -0500

    update vg docker

--- src/toil_vg/vg_config.py
+++ src/toil_vg/vg_config.py
@@ -157,7 +157,7 @@ container: """ + (default_container) + """
 ##   of through docker. 
 
 # Docker image to use for vg
-vg-docker: 'quay.io/vgteam/vg:v1.21.0-115-gf1eb801db-t351-run'
+vg-docker: 'quay.io/vgteam/vg:v1.21.0-132-g46fe0722f-t353-run'
 
 # Docker image to use for bcftools
 bcftools-docker: 'quay.io/biocontainers/bcftools:1.9--h4da6232_0'
@@ -431,7 +431,7 @@ container: """ + (default_container) + """
 ##   of through docker. 
 
 # Docker image to use for vg
-vg-docker: 'quay.io/vgteam/vg:v1.21.0-115-gf1eb801db-t351-run'
+vg-docker: 'quay.io/vgteam/vg:v1.21.0-132-g46fe0722f-t353-run'
 
 # Docker image to use for bcftools
 bcftools-docker: 'quay.io/biocontainers/bcftools:1.9--h4da6232_0'
