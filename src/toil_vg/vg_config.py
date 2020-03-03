69d8e922d2b693c17c071c2e302ce8146fabd870
Adam Novak <anovak@soe.ucsc.edu>
2020-03-03 14:01:45 -0800

    Switch to latest vg release, with compatible instructions

--- src/toil_vg/vg_config.py
+++ src/toil_vg/vg_config.py
@@ -156,7 +156,7 @@ container: """ + (default_container) + """
 ##   of through docker. 
 
 # Docker image to use for vg
-vg-docker: 'quay.io/vgteam/vg:v1.21.0-132-g46fe0722f-t353-run'
+vg-docker: 'quay.io/vgteam/vg:v1.22.0'
 
 # Docker image to use for bcftools
 bcftools-docker: 'quay.io/biocontainers/bcftools:1.9--h4da6232_0'
@@ -430,7 +430,7 @@ container: """ + (default_container) + """
 ##   of through docker. 
 
 # Docker image to use for vg
-vg-docker: 'quay.io/vgteam/vg:v1.21.0-132-g46fe0722f-t353-run'
+vg-docker: 'quay.io/vgteam/vg:v1.22.0'
 
 # Docker image to use for bcftools
 bcftools-docker: 'quay.io/biocontainers/bcftools:1.9--h4da6232_0'
