ed7fe342713a5687ecf705e596b27aa8de6e3fe9
Adam Novak <anovak@soe.ucsc.edu>
2024-02-07 17:35:22 -0500

    Log errors better and update Tidyverse

--- src/toil_vg/vg_config.py
+++ src/toil_vg/vg_config.py
@@ -188,7 +188,7 @@ rtg-docker: 'realtimegenomics/rtg-tools:3.8.4'
 pigz-docker: 'quay.io/glennhickey/pigz:latest'
 
 # Docker image to use to run R scripts
-r-docker: 'rocker/tidyverse:3.5.1'
+r-docker: 'rocker/tidyverse:4.3.2'
 
 # Docker image to use for vcflib
 vcflib-docker: 'quay.io/biocontainers/vcflib:1.0.0_rc1--0'
@@ -518,7 +518,7 @@ rtg-docker: 'realtimegenomics/rtg-tools:3.8.4'
 pigz-docker: 'quay.io/glennhickey/pigz:latest'
 
 # Docker image to use to run R scripts
-r-docker: 'rocker/tidyverse:3.5.1'
+r-docker: 'rocker/tidyverse:4.3.2'
 
 # Docker image to use for vcflib
 vcflib-docker: 'quay.io/biocontainers/vcflib:1.0.0_rc1--0'
